33170ffce34415e1e5a39b8f26db2b95cc2ca740
dipuhasan <depu1994@gmail.com>
2015-08-24 14:51:33 +0600

    Create Count_Num_Of_Binary_Strings.cpp

---
+++ Count_Num_Of_Binary_Strings.cpp
@@ -0,0 +1,27 @@
+//Count number of binary strings without consecutive 1’s
+
+#include <iostream>
+using namespace std;
+ 
+int countStrings(int n)
+{
+    int a[n], b[n];
+    a[0] = b[0] = 1;
+    for (int i = 1; i < n; i++)
+    {
+        a[i] = a[i-1] + b[i-1];
+        b[i] = a[i-1];
+    }
+    return a[n-1] + b[n-1];
+}
+ 
+
+int main()
+{
+    cout << countStrings(3) << endl;
+    return 0;
+}
+
+
+
+Output : 5
