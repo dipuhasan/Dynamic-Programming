1b6c75837ff16c9485c5155fa799010bee731377
dipuhasan <depu1994@gmail.com>
2015-08-24 15:08:56 +0600

    Create Count_nth_stairs.cpp

---
+++ Count-Nth-stairs/Count_nth_stairs.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+
+using namespace std;
+int fib(int n)
+{
+    if(n<=1)
+        return n;
+    return fib(n-1)+fib(n-2);
+}
+int countWays(int s)
+{
+    return fib(s+1);
+}
+
+int main()
+{
+
+    int s = 4;
+    printf("Number of ways  = %d\n", countWays(s));
+
+
+    return 0;
+}
+output : Number of ways = 5
+
+/*---------------------------------------*/
+// A C program to count number of ways to reach n't stair when
+// a person can climb either 1 or 2 stairs at a time
+#include<stdio.h>
+ 
+// A recursive function used by countWays
+int countWaysUtil(int n, int m)
+{
+    if (n <= 1)
+        return n;
+    int res = 0;
+    for (int i = 1; i<=m && i<=n; i++)
+        res += countWaysUtil(n-i, m);
+    return res;
+}
+ 
+// Returns number of ways to reach s'th stair
+int countWays(int s, int m)
+{
+    return countWaysUtil(s+1, m);
+}
+ 
+int main ()
+{
+    int s = 4, m = 2;
+    printf("Nuber of ways = %d", countWays(s, m));
+    return 0;
+}
+/*-----------------------------*/
+
+// A C program to count number of ways to reach n't stair when
+// a person can climb 1, 2, ..m stairs at a time
+#include<stdio.h>
+ 
+// A recursive function used by countWays
+int countWaysUtil(int n, int m)
+{
+    int res[n];
+    res[0] = 1; res[1] = 1;
+    for (int i=2; i<n; i++)
+    {
+       res[i] = 0;
+       for (int j=1; j<=m && j<=i; j++)
+         res[i] += res[i-j];
+    }
+    return res[n-1];
+}
+ 
+
+int countWays(int s, int m)
+{
+    return countWaysUtil(s+1, m);
+}
+ 
+int main ()
+{
+    int s = 4, m = 2;
+    printf("Nuber of ways = %d", countWays(s, m));
+    return 0;
+}
